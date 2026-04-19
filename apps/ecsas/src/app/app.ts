b700d615fcc9850ebd3d92e64bb1caab37b949e6
warnerb47 <taphagaye2001.mg@gmail.com>
2026-04-19 11:41:45 +0000

    feat: init dashboard component

--- apps/ecsas/src/app/app.ts
+++ apps/ecsas/src/app/app.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcome } from './nx-welcome';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [RouterModule],
   selector: 'ecsas-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
