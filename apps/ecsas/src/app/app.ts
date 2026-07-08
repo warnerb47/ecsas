7b9e386bfb0e92cda138824344fa618f81f9af31
warnerb47 <taphagaye2001.mg@gmail.com>
2026-07-08 23:05:56 +0000

    feat: add llama service

--- apps/ecsas/src/app/app.ts
+++ apps/ecsas/src/app/app.ts
@@ -1,23 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { LlamaService } from '@org/api/products';
 import { SidebarComponent } from '@org/ecsas/shared-ui';
+import { Command, Child } from '@tauri-apps/plugin-shell';
 
 @Component({
   imports: [RouterModule, SidebarComponent],
   selector: 'ecsas-root',
   templateUrl: './app.html',
 })
-export class App implements OnInit {
+export class App implements OnInit, OnDestroy {
   protected title = 'ecsas';
-  private readonly _llamaService = new LlamaService();
+  private _llamaService = new LlamaService();
 
   async ngOnInit() {
-    const init = await this._llamaService.initServer();
-    console.log({init});
-    const response = this._llamaService.test();
-    console.log({response});
-
+    await this._llamaService.startLlamaServer();
   }
+
+  ngOnDestroy() {
+    this._llamaService.stopLlamaServer();
+  }
+
 
 }
--- libs/api/products/src/lib/llama-service/llama.service.ts
+++ libs/api/products/src/lib/llama-service/llama.service.ts
@@ -1,39 +1,73 @@
-import { invoke } from '@tauri-apps/api/core';
 import { readFile } from '@tauri-apps/plugin-fs';
-
-
+import { Child, Command } from '@tauri-apps/plugin-shell';
 
 export class LlamaService {
   private apiUrl = 'http://localhost:8080/v1/chat/completions';
+  private llamaProcess: Child | null = null;
 
-  async initServer() {
-    // Trigger Rust command to start sidecar
-    return await invoke('start_ocr_server');
+  async startLlamaServer() {
+    try {
+      const sidecar = 'binaries/llama-server/llama-server';
+      const args = [
+        '-m',
+        './resources/Qwen3VL-2B-Instruct-Q8_0.gguf',
+        '--mmproj',
+        './resources/mmproj-Qwen3VL-2B-Instruct-F16.gguf',
+        '--port',
+        '8080',
+        '-t',
+        '8',
+        '--ctx-size',
+        '4096',
+        '--mlock',
+      ];
+
+      const command = Command.sidecar(sidecar, args);
+      const llamaProcess = await command.spawn();
+      this.llamaProcess = llamaProcess;
+      console.log(
+        `llama-server running on port 8080 (pid: ${this.llamaProcess?.pid})`,
+      );
+    } catch (error) {
+      console.error(`Failed to start: ${error}`);
+    }
+  }
+
+  stopLlamaServer() {
+    if (this.llamaProcess) {
+      console.log('Stopping llama-server...');
+      try {
+        this.llamaProcess?.kill();
+        this.llamaProcess = null;
+      } catch (err) {
+        console.error('Failed to kill process:', err);
+      }
+    }
   }
 
   async test() {
-
+    console.log('Testing llama-server...');
     const response = await fetch(this.apiUrl, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        model: "Qwen3VL-2B-Instruct-Q8_0",
-        messages: [{
-          role: "user",
-          content: [
-            { type: "text", text: "What is tauri" }
-          ]
-        }],
+        model: 'Qwen3VL-2B-Instruct-Q8_0',
+        messages: [
+          {
+            role: 'user',
+            content: [{ type: 'text', text: 'What is tauri' }],
+          },
+        ],
         temperature: 0.1,
-        top_k: 1
-      })
+        top_k: 1,
+      }),
     });
 
     const data = await response.json();
     return data;
   }
 
-  async extractIdCard(imagePath: string): Promise<any> {
+  async extractIdCard(imagePath: string): Promise<unknown> {
     // Read file as Base64 (Angular/TS)
     const base64Image = await this.fileToBase64(imagePath);
 
@@ -41,17 +75,25 @@ export class LlamaService {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        model: "Qwen3VL-2B-Instruct-Q8_0",
-        messages: [{
-          role: "user",
-          content: [
-            { type: "image_url", image_url: { url: `data:image/png;base64,${base64Image}` } },
-            { type: "text", text: "Extract all text fields from this ID card. Return ONLY valid JSON" }
-          ]
-        }],
+        model: 'Qwen3VL-2B-Instruct-Q8_0',
+        messages: [
+          {
+            role: 'user',
+            content: [
+              {
+                type: 'image_url',
+                image_url: { url: `data:image/png;base64,${base64Image}` },
+              },
+              {
+                type: 'text',
+                text: 'Extract all text fields from this ID card. Return ONLY valid JSON',
+              },
+            ],
+          },
+        ],
         temperature: 0.1,
-        top_k: 1
-      })
+        top_k: 1,
+      }),
     });
 
     const data = await response.json();
@@ -60,10 +102,12 @@ export class LlamaService {
 
   private fileToBase64(path: string): Promise<string> {
     return new Promise((resolve, reject) => {
-      readFile(path).then(data => {
-        const binary = String.fromCharCode(...new Uint8Array(data));
-        resolve(btoa(binary));
-      }).catch(reject);
+      readFile(path)
+        .then((data) => {
+          const binary = String.fromCharCode(...new Uint8Array(data));
+          resolve(btoa(binary));
+        })
+        .catch(reject);
     });
   }
 }
