41ed4082f19e7a87752282d18a42d292439afbc5
warnerb47 <taphagaye2001.mg@gmail.com>
2026-07-10 20:23:39 +0000

    clean up app.ts

--- apps/ecsas/src/app/app.ts
+++ apps/ecsas/src/app/app.ts
@@ -1,6 +1,5 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { LlamaService } from '@org/api/products';
 import { SidebarComponent } from '@org/ecsas/shared-ui';
 
 @Component({
@@ -8,17 +7,6 @@ import { SidebarComponent } from '@org/ecsas/shared-ui';
   selector: 'ecsas-root',
   templateUrl: './app.html',
 })
-export class App implements OnInit, OnDestroy {
+export class App {
   protected title = 'ecsas';
-  private _llamaService = new LlamaService();
-
-  async ngOnInit() {
-    await this._llamaService.startLlamaServer();
-  }
-
-  ngOnDestroy() {
-    this._llamaService.stopLlamaServer();
-  }
-
-
 }
