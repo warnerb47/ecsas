72c8945b9ecbbdd70737e2dc6276aa7e88220e6c
warnerb47 <taphagaye2001.mg@gmail.com>
2026-07-23 23:13:17 +0000

    feat: add user btn to launch or stop llama-server

--- libs/ecsas/feature-dashboard/src/lib/dashboard/dashboard.component.ts
+++ libs/ecsas/feature-dashboard/src/lib/dashboard/dashboard.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { ButtonComponent, TopbarComponent } from '@org/ecsas/shared-ui';
 import { StatisticComponent } from '../components/statistic/statistic.component';
 import { PendingProcedureComponent } from '../components/pending-procedure/pending-procedure.component';
 import { RecentApplicationComponent } from '../components/recent-application/recent-application.component';
 import { RouterLink } from '@angular/router';
+import { LlamaService } from '@org/api/products';
 
 @Component({
   selector: 'lib-dashboard.component',
   imports: [RouterLink, TopbarComponent, StatisticComponent, PendingProcedureComponent, RecentApplicationComponent, ButtonComponent],
   templateUrl: './dashboard.component.html',
 })
-export class DashboardComponent {}
+export class DashboardComponent implements OnInit {
+  private readonly _llamaService = new LlamaService();
+  isRunning = signal(false);
+
+  ngOnInit() {
+    this.updateState();
+  }
+
+  async updateState() {
+    const isRunning = await this._llamaService.isRunning()
+    this.isRunning.set(isRunning);
+  }
+
+  async stopLlamaServer() {
+    await this._llamaService.stopLlamaServer();
+    this.updateState();
+  }
+
+  async startLlamaServer() {
+    await this._llamaService.startLlamaServer();
+    this.updateState();
+  }
+}
