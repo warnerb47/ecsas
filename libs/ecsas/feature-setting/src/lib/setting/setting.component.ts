d61a7737097a6709eb69b694692b40b7ad7a077b
warnerb47 <taphagaye2001.mg@gmail.com>
2026-09-01 20:38:43 +0000

    feat: update event-detail design

--- libs/ecsas/feature-setting/src/lib/setting/setting.component.ts
+++ libs/ecsas/feature-setting/src/lib/setting/setting.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { ExportZipCardComponent } from '../components/export-zip-card/export-zip-card.component';
-import { ProfileCardComponent } from '../components/profile-card/profile-card.component';
 
 @Component({
   selector: 'lib-setting',
-  imports: [ProfileCardComponent, ExportZipCardComponent],
+  imports: [ ExportZipCardComponent],
   templateUrl: './setting.component.html',
 })
 export class SettingComponent {}
